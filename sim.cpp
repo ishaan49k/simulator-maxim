492c5a84d0386c39eae307613c44f96a79233a66
Ishaan Kulkarni <ishaan.kulkarni049@gmail.com>
2025-11-17 22:17:17 +0530

    Initial commit

---
+++ sim.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+enum State{
+    highlatency = 1,
+    lowlatency = 2,
+    shutdown = 3
+}
+
+
+
+
+class provider{
+    string name;
+    State s;
+}
+
+
+
+class stats{
+    int latency;
+    bool error;
+    int iptoken;
+    int optoken;
+}
+
+
+
+
+
+
+stats Simulator(vector<providers> p, provider p1, State s1, bool timout){
+
+    
+
+
+
+
+}
+
+
+
+
+
+
+int main() {
+
+
+}
+
+
+
