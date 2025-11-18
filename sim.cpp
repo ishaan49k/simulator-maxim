33dd08a646aa67265988368211a3d03fd8cfeed3
Ishaan Kulkarni <ishaan.kulkarni049@gmail.com>
2025-11-19 00:19:31 +0530

    changes

--- sim.cpp
+++ sim.cpp
@@ -3,42 +3,110 @@ using namespace std;
 
 
 
-enum State{
-    highlatency = 1,
-    lowlatency = 2,
-    shutdown = 3
-}
+enum class Scenario {
+    LowLatency,
+    HighLatency,
+    PartialLatency,
+    Shutdown,
+    Blackout
+};
 
 
 
 
 class provider{
     string name;
-    State s;
-}
+    Scenario currentstate;
+    int disabletime;
+};
 
 
 
-class stats{
-    int latency;
-    bool error;
+struct Stats {
     int iptoken;
     int optoken;
+    int latency;
+    bool error;
+};
+
+
+
+
+
+
+
+
+class Simulator {
+public:
+    Simulator(Scenario scenario, int numProviders, int timeoutMs) {
+        this->scenario     = scenario;
+        this->numProviders = numProviders;
+        this->timeoutMs    = timeoutMs;
+    }
+
+
+    // multiple provider simulation
+    vector<Stats> simulate(){
+        vector<Stats> results;
+        results.reserve(numProviders);
+
+        for (int i = 0; i < numProviders; i++) {
+            results.push_back(simulateOne());
+        }
+
+        return results;
 }
 
 
 
+private:
+    Scenario scenario;
+    int numProviders;
+    int timeoutMs;
 
+    // simulate one provider for the selected scenario
+    Stats simulateOne() {
+        Stats st;
+        st.iptoken = rand() % 100 + 1;
+        st.optoken = rand() % 50 + 1;
 
+        switch (scenario) {
 
-stats Simulator(vector<providers> p, provider p1, State s1, bool timout){
+            case Scenario::LowLatency:
+                st.latency = rand() % 40 + 10;
+                st.error = false;
+                break;
 
-    
+            case Scenario::HighLatency:
+                st.latency = rand() % 300 + 200;
+                st.error = (st.latency > timeoutMs);
+                break;
 
+            case Scenario::PartialLatency:
+                if (rand() % 2 == 0) {
+                    st.latency = rand() % 50 + 20;
+                    st.error = false;
+                } else {
+                    st.latency = rand() % 300 + 150;
+                    st.error = (st.latency > timeoutMs);
+                }
+                break;
 
+            case Scenario::Shutdown:
+                st.latency = 0;
+                st.error = true;
+                break;
 
+            case Scenario::Blackout:
+                st.latency = timeoutMs + 300;
+                st.error = true;
+                break;
+        }
 
-}
+        return st;
+    }
+};
+
 
 
 
@@ -46,9 +114,20 @@ stats Simulator(vector<providers> p, provider p1, State s1, bool timout){
 
 
 int main() {
+    srand(time(NULL));
+    Simulator sim(Scenario::HighLatency, 5, 200);
 
+    auto results = sim.simulate();
 
+    for (int i = 0; i < results.size(); i++) {
+        cout << "provider " << i+1 << ":" << endl;
+        cout << "  ip token: " << results[i].iptoken << endl;
+        cout << "  op token: " << results[i].optoken << endl;
+        cout << "  latency: " << results[i].latency << " ms" << endl;
+        cout << "  error: " << (results[i].error ? "Yes" : "No") << endl;
+    }
+
+    return 0;
 }
-
 
 
